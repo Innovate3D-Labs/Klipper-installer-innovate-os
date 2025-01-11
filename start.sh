5a1667ce02008566c22f72a5d566faa6e8c8c698
EinsPommes <jannikkugler2006@gmail.com>
2025-01-11 10:34:59 +0100

    start sh

--- start.sh
+++ start.sh
@@ -3,5 +3,9 @@
 # Aktiviere die Python-Umgebung
 source venv/bin/activate
 
+# Setze den Python-Path
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
 # Starte den Backend-Server
-python -m src.backend.main
+cd src/backend
+uvicorn main:app --host 0.0.0.0 --port 8000
