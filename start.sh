5f02c524185778a40d8858479526c06849548f94
EinsPommes <jannikkugler2006@gmail.com>
2025-01-11 10:37:24 +0100

    wechseln in das src Verzeichnis

--- start.sh
+++ start.sh
@@ -3,9 +3,6 @@
 # Aktiviere die Python-Umgebung
 source venv/bin/activate
 
-# Setze den Python-Path
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-
 # Starte den Backend-Server
-cd src/backend
-uvicorn main:app --host 0.0.0.0 --port 8000
+cd src
+uvicorn backend.main:app --host 0.0.0.0 --port 8000 --reload
