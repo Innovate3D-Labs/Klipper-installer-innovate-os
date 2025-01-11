18995d2b1ed799bbd9024fbfede166e43701092b
EinsPommes <jannikkugler2006@gmail.com>
2025-01-11 10:43:18 +0100

    /app

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@
 source venv/bin/activate
 
 # Starte den Backend-Server
-PYTHONPATH=$PWD/src uvicorn backend.main:app --host 0.0.0.0 --port 8000 --reload
+cd src/backend
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
